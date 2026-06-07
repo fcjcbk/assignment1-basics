ca0b4dd22edcd2822399c85a2b980b92c50e8dd7
fcjcbk <1097086201@qq.com>
2026-06-08 00:10:39 +0800

    Add CheckPoint

--- cs336_basics/model/funtional.py
+++ cs336_basics/model/funtional.py
@@ -1,3 +1,6 @@
+import os
+from typing import IO, Any, BinaryIO
+
 from torch import Tensor
 import torch
 import einx
@@ -188,3 +191,53 @@ def sample_train_data(
     inputs_tensor = torch.tensor(np.stack(inputs), dtype=torch.long, device=device)
     labels_tensor = torch.tensor(np.stack(labels), dtype=torch.long, device=device)
     return inputs_tensor, labels_tensor
+
+def save_checkpoint(
+    model: torch.nn.Module,
+    optimizer: torch.optim.Optimizer,
+    iteration: int,
+    out: str | os.PathLike | BinaryIO | IO[bytes],
+):
+    """
+    Given a model, optimizer, and an iteration number, serialize them to disk.
+
+    Args:
+        model (torch.nn.Module): Serialize the state of this model.
+        optimizer (torch.optim.Optimizer): Serialize the state of this optimizer.
+        iteration (int): Serialize this value, which represents the number of training iterations
+            we've completed.
+        out (str | os.PathLike | BinaryIO | IO[bytes]): Path or file-like object to serialize the model, optimizer, and iteration to.
+    """
+    model_state = model.state_dict()
+    optimizer_state = optimizer.state_dict()
+    
+    state = dict()
+    state['model_state'] = model_state
+    state['optimizer_state'] = optimizer_state
+    state['iteration'] = iteration
+    torch.save(state, out)
+
+
+def load_checkpoint(
+    src: str | os.PathLike | BinaryIO | IO[bytes],
+    model: torch.nn.Module,
+    optimizer: torch.optim.Optimizer,
+) -> int:
+    """
+    Given a serialized checkpoint (path or file-like object), restore the
+    serialized state to the given model and optimizer.
+    Return the number of iterations that we previously serialized in
+    the checkpoint.
+
+    Args:
+        src (str | os.PathLike | BinaryIO | IO[bytes]): Path or file-like object to serialized checkpoint.
+        model (torch.nn.Module): Restore the state of this model.
+        optimizer (torch.optim.Optimizer): Restore the state of this optimizer.
+    Returns:
+        int: the previously-serialized number of iterations.
+    """
+    state = torch.load(src)
+    model.load_state_dict(state['model_state'])
+    optimizer.load_state_dict(state['optimizer_state'])
+    iteration = state['iteration']
+    return iteration
